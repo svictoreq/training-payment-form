8bf6de2bc9e155eb9083af70210a4ed716350450
Victor Requena <svictoreq@gmail.com>
2018-03-17 17:10:52 -0400

    Add style to the output section of the form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	// Payment method selector code
+	const paypalLogo = '<img src="img/paypal-logo-full.svg">';
+	const cardButton = `<i class="material-icons">lock_outline</i>
+				<span>Finish and pay</span>`
+
+	const paySelectors = $('input[type="radio"]');
+	const formButton = $('form button');
+	$(paySelectors).on('click', function() {
+		if ( $(this).val() === 'paypal' ) {
+			$(formButton).removeClass('button-card');
+			$(formButton).addClass('button-paypal');
+			$(formButton).html(paypalLogo);
+		} else {
+			$(formButton).removeClass('button-paypal');
+			$(formButton).addClass('button-card');
+			$(formButton).html(cardButton);
+		}
+	});
+
+	// Mobile card list code
 	const mobileCardList = $('.mobile-card-list');
 	const card = $('.mobile-card-wrapper .card');
 	const icon = $('.mobile-card-wrapper i');
